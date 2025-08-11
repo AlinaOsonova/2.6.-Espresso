0046c6f846bad5fcbe65a7890472d7e945f12fbf
Alina <eparis@atlassian.com>
2025-08-11 13:27:08 +0300

    first task

---
+++ app/src/main/java/ru/kkuzmichev/simpleappforespresso/ui/gallery/GalleryItem.java
@@ -0,0 +1,37 @@
+package ru.kkuzmichev.simpleappforespresso.ui.gallery;
+
+public class GalleryItem {
+    private String title;
+    private String description;
+    private int number;
+
+    public GalleryItem(String title, String description, int number) {
+        this.title = title;
+        this.description = description;
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
